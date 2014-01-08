5a8894f1e9d175bbac6e063ee96f3040c8da3047
Prateek Saxena <prtksxna@gmail.com>
2014-01-08 17:35:05 +0530

    Restructure BoardGen

--- src/js/board_gen.js
+++ src/js/board_gen.js
@@ -1,62 +1,63 @@
-var BoardGen = {
-    init: function() {
-        this.letters = [];
-        this.populate();
-    },
+var BoardGen = function(){
 
-    populate: function() {
-        this.pushLetter("e", 850);
-        this.pushLetter("t", 850);
-        this.pushLetter("a", 700);
-        this.pushLetter("o", 701);
-        this.pushLetter("n", 674);
-        this.pushLetter("i", 650);
-        this.pushLetter("h", 609);
-        this.pushLetter("r", 599);
-        this.pushLetter("s", 550);
-        this.pushLetter("d", 425);
-        this.pushLetter("l", 402);
-        this.pushLetter("c", 278);
-        this.pushLetter("u", 255);
-        this.pushLetter("m", 241);
-        this.pushLetter("w", 236);
-        this.pushLetter("f", 223);
-        this.pushLetter("g", 201);
-        this.pushLetter("y", 196);
-        this.pushLetter("p", 192);
-        this.pushLetter("b", 149);
-        this.pushLetter("v", 130);
-        this.pushLetter("k", 130);
-        this.pushLetter("q", 100);
-        this.pushLetter("j", 100);
-        this.pushLetter("z", 60);
-        this.pushLetter("x", 60);
-    },
+    letters = [];
+    populate();
 
-    pushLetter: function(l, t) {
-        for(var i = 0; i < t; i++) {
-            this.letters.push(l);
-        }
-    },
-
-    getBoard: function() {
-        var o = this.letters;
-        var v = ["a", "e", "i", "o", "u"];
-        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-        o = o.slice(0,25);
-
-        if(o.indexOf("q") > -1 && o.indexOf("u") < 0){
-            o[Math.floor(Math.random() * 25)] = "u";
-        }
-
-        var b = [];
-
-        for(var i = 0; i < o.length; i++) {
-            var e = o[i];
-            var av = (v.indexOf(e) > -1) ? 5 : 3;
-            b.push([e, av]);
-        }
-
-        return b;
+    function populate() {
+        pushLetter("e", 850);
+        pushLetter("t", 850);
+        pushLetter("a", 700);
+        pushLetter("o", 701);
+        pushLetter("n", 674);
+        pushLetter("i", 650);
+        pushLetter("h", 609);
+        pushLetter("r", 599);
+        pushLetter("s", 550);
+        pushLetter("d", 425);
+        pushLetter("l", 402);
+        pushLetter("c", 278);
+        pushLetter("u", 255);
+        pushLetter("m", 241);
+        pushLetter("w", 236);
+        pushLetter("f", 223);
+        pushLetter("g", 201);
+        pushLetter("y", 196);
+        pushLetter("p", 192);
+        pushLetter("b", 149);
+        pushLetter("v", 130);
+        pushLetter("k", 130);
+        pushLetter("q", 100);
+        pushLetter("j", 100);
+        pushLetter("z", 60);
+        pushLetter("x", 60);
     }
-}
+
+    function pushLetter(l, t) {
+        for(var i = 0; i < t; i++) {
+            letters.push(l);
+        }
+    }
+
+    return {
+        getBoard: function() {
+            var o = letters;
+            var v = ["a", "e", "i", "o", "u"];
+            for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+            o = o.slice(0,25);
+
+            if(o.indexOf("q") > -1 && o.indexOf("u") < 0){
+                o[Math.floor(Math.random() * 25)] = "u";
+            }
+
+            var b = [];
+
+            for(var i = 0; i < o.length; i++) {
+                var e = o[i];
+                var av = (v.indexOf(e) > -1) ? 5 : 3;
+                b.push([e, av]);
+            }
+
+            return b;
+        }
+    }
+}();
