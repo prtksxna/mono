5ab3bf0cd898cebb8666be07039803729bb3026a
Prateek Saxena <prtksxna@gmail.com>
2014-01-08 12:19:36 +0530

    Add initial design of the menu

--- src/js/mono.js
+++ src/js/mono.js
@@ -4,10 +4,29 @@ var mono = {
     },
 
     initUI: function() {
+        this.resizeUI();
+        $("#menu h1").addClass("active");
+        $("#menu ul li a").addClass("active");
         $("#game").hide();
+    },
+
+    resizeUI: function() {
+        if($(document).width() < $(document).height()){
+            $("#container").removeClass("compress");
+            $("body").css("font-size", ($(window).height()/100.0));
+        }else{
+            $("#container").addClass("compress");
+            $("body").css("font-size", ($("#container").height()/100.0));
+        }
     }
 };
 
 $(function() {
     mono.init();
+});
+
+
+$(window).resize(function(){
+    console.log(1);
+    mono.resizeUI();
 });
