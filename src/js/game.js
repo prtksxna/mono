4b2e966e9c9018db1481cf04e5ab7d67e3cbbcec
Prateek Saxena <prtksxna@gmail.com>
2014-01-08 17:51:59 +0530

    Add tile

--- src/js/game.js
+++ src/js/game.js
@@ -1,8 +1,39 @@
 var Game = {
     init: function(endless) {
+        var self = this;
         this.endless = endless;
 
         $("#menu").hide();
         $("#game").show();
+
+        this.makeBoard();
+        this.tiles = [];
+        this.playing = [];
+
+        $("#del").on("click", function() {
+            self.del();
+        });
+    },
+
+    makeBoard: function() {
+        var t = BoardGen.getBoard();
+        $("#stack td").each(function(i) {
+            var tile = new Tile(t[i], this);
+        });
+    },
+
+    playLetter: function(tile) {
+        this.playing.push(tile);
+        $("#word").append($("<span>").text(tile.al));
+    },
+
+    del: function() {
+        if(this.playing.length === 0) return false;
+
+        var t = this.playing.pop();
+        t.used(false);
+
+        $("#word span").last().remove();
+        return t;
     }
 }
